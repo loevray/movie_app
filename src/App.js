042ee15b6ad02988c3ff16a053d9896da309f740
loevray <87127340+loevray@users.noreply.github.com>
2021-08-19 17:29:59 +0900

    react done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie'
 
-function Potato() {
-  return <h1>I like Potato</h1>;
-}
-
-function App() {
-  return (
-    <div>
-      <h1>hello</h1>
-      <Potato />
-    </div>
-  );
-}
+class App extends React.Component{
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getMovies = async() =>{
+    const {
+      data: { 
+        data: {movies}
+      }
+    } = await axios.get("https://yts.mx/api/v2/list_movies.json");
+    this.setState({ movies, isLoading: false });
+  }
+  componentDidMount() {
+    this.getMovies();
+  }
+    render() {
+      const { isLoading, movies } = this.state;
+      return (
+      <section class="container">
+        {isLoading ? (
+        <div class="loader">
+          <span class="loader__tex">Loading...</span>
+        </div>
+        ) : (
+          <div class="movies">
+            {movies.map(movie => (
+          <Movie
+            key={movie.id} 
+            id={movie.id} 
+            year={movie.year} 
+            title={movie.title} 
+            summary={movie.summary} 
+            poster={movie.medium_cover_image}
+          />
+      ))}
+          </div>
+        )}
+      </section>
+      )
+    }
+  }
 
 export default App;
